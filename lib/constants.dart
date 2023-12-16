37c0b40211617859b7e46fca61c2a4440e37bed5
duytruong-23 <zduytruongz@gmail.com>
2023-12-16 09:55:25 +0700

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const String ACCESS_TOKEN = "sk.eyJ1IjoiZHV5dHJ1b25nMjMiLCJhIjoiY2xxMzZoendmMDhrMjJqcDh0NWZscnh3eCJ9.bmv55PkVcpdicBjwYNLZcw";
+  static const String PUBLIC_TOKEN = "pk.eyJ1IjoiZHV5dHJ1b25nMjMiLCJhIjoiY2xxMzZmMHRwMDlrcjJqcnNhcHlkd29udiJ9.jMqOTomveRzTEZrNkJzluA";
+}
